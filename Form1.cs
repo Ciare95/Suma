ccb82d3c0851ababb33e887876bd876d91080de5
Ciare95 <100888282+Ciare95@users.noreply.github.com>
2025-08-05 11:21:30 -0500

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Suma
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String seleccion1 = textBox1.Text;
+            int numero1 = Convert.ToInt32(seleccion1);
+
+            String seleccio2 = textBox2.Text;
+            int numero2 = Convert.ToInt32(seleccio2);
+
+            int resultado = numero1 + numero2;
+
+            MessageBox.Show(resultado.ToString());
+        }
+    }
+}
